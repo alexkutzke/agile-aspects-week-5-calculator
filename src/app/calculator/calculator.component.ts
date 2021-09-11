92005bb9f48df81212e6f2de9d1db9a1c680a542
Alexandre Stephani <37486850+Ale-Ste@users.noreply.github.com>
2021-09-11 16:50:48 -0300

    inseridos os métodos sub e div

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { SumService } from '../sum.service';
 import { MultiplyBySumService } from '../multiply-by-sum.service';
 import { PowerByMultiplyService } from '../power-by-multiply.service';
+import { SubService } from '../sub.service';
+import { DivBySubService } from '../div-by-sub.service';
 
 @Component({
   selector: 'app-calculator',
@@ -14,11 +16,15 @@ export class CalculatorComponent implements OnInit {
   private sumRes: number = 0;
   private multRes: number = 0;
   private powRes: number = 0;
+  private subRes: number = 0;
+  private divRes: number = 0;
 
   constructor(
     private sumService: SumService,
     private multiplyBySumService: MultiplyBySumService,
-    private powerByMultiplyService: PowerByMultiplyService
+    private powerByMultiplyService: PowerByMultiplyService,
+    private subService: SubService,
+    private divBySubService: DivBySubService
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +37,9 @@ export class CalculatorComponent implements OnInit {
     this.sum(num1,num2);
     this.multiply(num1,num2);
     this.power(num1,num2);
+    this.sub(num1,num2);
+    this.divide(num1,num2);
+    
   }
 
   sum(num1: number, num2: number) {
@@ -45,6 +54,14 @@ export class CalculatorComponent implements OnInit {
     this.powRes = this.powerByMultiplyService.power(num1,num2);
   }
 
+  sub(num1: number, num2: number) {
+    this.subRes = this.subService.sub(num1,num2);
+  }
+
+  divide(num1: number, num2: number) {
+    this.divRes = this.divBySubService.divide(num1,num2);
+  }
+
   get sumResult() {
     return this.sumRes;
   }
@@ -55,6 +72,14 @@ export class CalculatorComponent implements OnInit {
 
   get powResult() {
     return this.powRes;
+  }
+
+  get subResult() {
+    return this.subRes;
+  }
+
+  get divResult() {
+    return this.divRes;
   }
 
 }
